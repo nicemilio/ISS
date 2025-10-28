c505752536cf3a68a37282db32a3d5694a976577
nicemilio <nic.th@web.de>
2025-10-28 17:24:12 +0100

    Added exercise 5

--- Uebungsblatt_02/code/given/sheet02.py
+++ Uebungsblatt_02/code/given/sheet02.py
@@ -33,13 +33,23 @@ def filter_image(image: np.ndarray, kernel: np.ndarray):
     filtered_image = image.copy()
 
     height, width = filtered_image.shape[:2]
+    kHeight, kWidth = kernel.shape #Kernel Maße
+    padHeight, padWidth = kHeight // 2, kWidth // 2 #Padding Maße
     
     output = np.zeros((height, width), dtype=np.float32)
 
     filtered_image = None
 
-    # TODO: Ihre Lösung
-    # ...
+    channels = image.shape[2]
+    filtered_image = np.zeros_like(image, dtype=np.uint8)
+    for c in range(channels):
+        padded_image = np.pad(image[:, :, c], ((padHeight, padHeight), (padWidth, padWidth)))
+        output = np.zeros((height, width), dtype=np.float32)
+        for i in range(height):
+            for j in range(width):
+                region = padded_image[i:i+kHeight, j:j+kWidth]
+                output[i, j] = np.sum(region * kernel)
+        filtered_image[:, :, c] = np.clip(output, 0, 255).astype(np.uint8)
     
     return filtered_image
 
@@ -59,14 +69,14 @@ if __name__ == "__main__":
 
     input_image = cv2.imread(join(image_folder, "Testbild_Werkzeuge_768x576.png"))
 
-    kernel_33 = None # TODO: Ihre Lösung
+    kernel_33 = np.ones((3, 3), dtype=np.float32) / 9.0
     result = filter_image(image=input_image, kernel=kernel_33)
     
     cv2.imshow("Ex. 5) 3x3 Mittelwertfilter", result)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
-    kernel_55 = None # TODO: Ihre Lösung
+    kernel_55 = np.ones((5, 5), dtype=np.float32) / 25.0
     result = filter_image(image=input_image, kernel=kernel_55)
     
     cv2.imshow("Ex. 5) 5x5 Mittelwertfilter", result)
