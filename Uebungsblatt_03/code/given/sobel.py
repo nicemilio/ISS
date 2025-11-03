5203ec0b0661988b31a92dbf78629eed1060052d
nicemilio <nic.th@web.de>
2025-11-03 14:00:09 +0100

    Added sobel functions

--- Uebungsblatt_03/code/given/sobel.py
+++ Uebungsblatt_03/code/given/sobel.py
@@ -1,17 +1,43 @@
 import numpy as np
 import cv2
+from scipy.signal import convolve2d
 
-SOBEL_X = None # TODO
-SOBEL_Y = None # TODO
+SOBEL_X = np.array([[-1, 0, 1],
+                    [-2, 0, 2],
+                    [-1, 0, 1]], dtype=np.float32)
+
+SOBEL_Y = np.array([[-1, -2, -1],
+                    [ 0,  0,  0],
+                    [ 1,  2,  1]], dtype=np.float32)
 
 def linear_stretch_to_u8(img: np.ndarray) -> np.ndarray:
-    # TODO
-    return None
+    # Minimum und Maximum des Bildes bestimmen
+    min_val = np.min(img)
+    max_val = np.max(img)
+
+    # Schutz vor Division durch 0 (falls konstantes Bild)
+    if max_val == min_val:
+        return np.zeros_like(img, dtype=np.uint8)
+
+    # Lineare Skalierung auf [0, 255]
+    stretched = (img - min_val) / (max_val - min_val) * 255.0
+
+    # In uint8 konvertieren
+    return stretched.astype(np.uint8)
 
 def sobel_gradients(gray_f32: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-    # TODO
-    return None, None
+
+    # 2D-Faltung (Rand mit Spiegelung behandeln)
+    Gx = convolve2d(gray_f32, SOBEL_X, mode="same", boundary="symm")
+    Gy = convolve2d(gray_f32, SOBEL_Y, mode="same", boundary="symm")
+
+    return Gx, Gy
 
 def magnitude_and_orientation(gx: np.ndarray, gy: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-    # TODO
-    return None, None
+    # Betrag nach euklidischer Norm
+    magnitude = np.sqrt(gx**2 + gy**2)
+
+    # Orientierung mit atan2 (liefert Winkel zwischen -π und π)
+    orientation = np.arctan2(gy, gx)
+
+    return magnitude, orientation
