c551aab353ac36d1e0a16795674faa96b5e488f7
Jacob C <s07jcani@uni-bonn.de>
2025-11-12 14:56:06 +0100

    added uebungscode 04

---
+++ uebungscode_04/examples/demo.cpp
@@ -0,0 +1,7 @@
+#include <meine_funktionen.hpp>
+
+int main() {
+  // Hier Sinus, Kosius, usw für einige Werte ausrechnen
+  // und Ergebnisse ausdrucken
+  
+}
---
+++ uebungscode_04/include/meine_funktionen.hpp
@@ -0,0 +1,25 @@
+#ifndef __MEINE_FUNKTIONEN_HPP__
+#define __MEINE_FUNKTIONEN_HPP__
+
+
+// Gute Kommentare hinzufügen
+
+
+double sinus(double x);
+
+double sinus(double x, double toleranzwert);
+
+double kosinus(double x);
+
+double kosinus(double x, double toleranzwert);
+
+double tangens(double x);
+
+double tangens(double x, double toleranzwert);
+
+double kotangens(double x);
+
+double kotangens(double x, double toleranzwert);
+
+
+#endif
