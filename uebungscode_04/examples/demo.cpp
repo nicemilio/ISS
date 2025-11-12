fcde379d68f84d7dc1e1a2b79cd2adfed5d6086c
Jacob C <s07jcani@uni-bonn.de>
2025-11-12 15:03:30 +0100

    deleted wrong worksheet

--- uebungscode_04/examples/demo.cpp
+++
@@ -1,7 +0,0 @@
-#include <meine_funktionen.hpp>
-
-int main() {
-  // Hier Sinus, Kosius, usw für einige Werte ausrechnen
-  // und Ergebnisse ausdrucken
-  
-}
--- uebungscode_04/src/meine_funktionen.cpp
+++
@@ -1,2 +0,0 @@
-#include <meine_funktionen.hpp>
-
